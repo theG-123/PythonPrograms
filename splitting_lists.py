7620b6188a10610ab0a4eff9f8caea90d4b970bd
theG-123 <153643437+theG-123@users.noreply.github.com>
2024-02-23 15:10:41 +0000

    splitting_lists still doesnt work but were close

---
+++ splitting_lists.py
@@ -0,0 +1,18 @@
+#Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() 
+#method. The program should build a list of words. For each word on each line check to see if the word is already in the
+#list and if not append it to the list. When the program completes, sort and print the resulting words in python sort()
+#order as shown in the desired output. You can download the sample data at http://www.py4e.com/code3/romeo.txt
+
+#Getting input from the user
+getFile = input ('What file would you like to open? ')
+
+try:
+    openFile = open(getFile)
+except:
+    print ('Error: Could not open file')
+    exit ()
+
+for line in openFile:
+    line = line.rstrip()
+    splittedFile = line.split()
+    print (splittedFile.sort())
