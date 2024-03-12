db27b5788b1c31b9ade9abfafea91187724d2aa9
theG-123 <153643437+theG-123@users.noreply.github.com>
2024-03-12 15:52:13 +0000

    advMax&MinNum.py now works

--- advMax&MinNum.py
+++ advMax&MinNum.py
@@ -1,15 +1,23 @@
 numList = []
+
 while True:
     prompt = input ('Enter a number: ')
 #check if the prompt is done, if it is then close the loop
     try:
-        int (prompt)
+        fprompt = float(prompt)
     except:
-        if prompt == 'done':
+        if prompt.lower() == 'done':
             break
         else:
             print ('Invalid input')
             continue
-    numList.append(prompt)
+    
+    numList.append(fprompt)
+
+    maxNumber = max(numList)
+    minNumber = min(numList)
 print (numList)
-print ('Maximum:', max(numList), '\nMinumum:', min(numList))
+if numList == []:
+    print('Error: No numbers entered')
+else:
+    print ('Maximum:', maxNumber, '\nMinumum:', minNumber)
