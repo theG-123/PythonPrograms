a6a288abc26e528e503c79f9e2e9820965a81764
theG-123 <153643437+theG-123@users.noreply.github.com>
2024-03-20 16:45:58 +0000

    dictionary01 now works. Next thing is to make the assignment that searches for many messages and check for the most active/repeated email out of all.

--- dictionary01.py
+++ dictionary01.py
@@ -1,7 +1,17 @@
-dictionary = {}
+#Using dicctionaries to get the number of how many times a word or something specifically is repeated in a file
+
 fileOpen = open ('romeo.txt')
+dictionary = {}
+repeatedNumber = 0
+
 for line in fileOpen:
     words = line.split()
     for word in words:
         dictionary[word] = dictionary.get(word,0) + 1
-print (dictionary)
+
+for key, value in dictionary.items():
+    if value > repeatedNumber:
+        repeatedNumber = value
+        mostRepeatedWord = key
+
+print (mostRepeatedWord, repeatedNumber)
