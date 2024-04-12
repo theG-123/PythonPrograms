14151e3c84e60ff6f3a88d3b90801d4f085c9af0
theG-123 <153643437+theG-123@users.noreply.github.com>
2024-04-12 03:46:12 +0000

    added comments fr readability,still need to finish

--- letterCounter.py
+++ letterCounter.py
@@ -26,20 +26,21 @@ while True: #Opening the file
                 dot_count += 1  # Increment the counter
                 time.sleep(0.4)  # Pause for .3 seconds
         print('\n')
-        break
+        break #get out of the loop if the file has been succesfully opened
     except:
         print('Error: File Could not be opened')
-        continue
+        continue #Print the error message and start again (ask for the file that you want to open)
 
 #Get the letters of the file and find how many times each letter is repeated
 letterFrequency = dict()
 for line in openFile:
     line = line.translate(str.maketrans('', '', string.punctuation)) #Getting rid of any type of punctuation
-    line = line.rstrip()
-    line = line.lower() # 
-    words = line.split()
-    for word in words:
+    line = line.rstrip() #Getting rid of the invisible characters in each line
+    line = line.lower() #Making everything lowercase so the dictionary is sorted properly
+    words = line.split() #Splitting each line in the file into words
+    for word in words: #loop through all the words and letters in each line 
         for letter in word:
-            letterFrequency [letter] = letterFrequency.get(letter,0) + 1
+            #add the letter to the list if it has not been added yet; if it has, updates its count plus one
+            letterFrequency [letter] = letterFrequency.get(letter,0) + 1 
 
 print (sorted(letterFrequency.items()))
