35d6d348682e5a2e797994966e5037b7ddb508e0
theG-123 <153643437+theG-123@users.noreply.github.com>
2024-04-21 16:27:32 +0000

    users are asked how many digits they want to print

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -1,9 +1,17 @@
 #Calculate the first 20 digits of the fibonacci sequence
 
+while True:
+    howManyNums = input('How many numbers from the fibbonaci sequence do you want to print out? ') 
+    try:
+        numbers = int (howManyNums)
+        break
+    except:
+        print('Please enter a number')
+        continue
 
 fibonnacci_seq = [0,1]
 
-for i in range (20):
+for i in range (numbers):
     fibonnacci_seq.append(fibonnacci_seq[-1] + fibonnacci_seq[-2])
 
 print (fibonnacci_seq)
